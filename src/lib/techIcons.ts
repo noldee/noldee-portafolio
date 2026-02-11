c13fa376eab4d39321e092432144c06b4eda2130
Walter Soca <188809273+noldee@users.noreply.github.com>
2026-02-11 16:43:00 -0500

    Portafolio

---
+++ src/lib/techIcons.ts
@@ -0,0 +1,14 @@
+export const techIcons: Record<string, string> = {
+  astro: "/images/icons/astro.svg",
+  html: "/images/icons/html.svg",
+  css: "/images/icons/css.svg",
+  bootstrap: "/images/icons/bootstrap.svg",
+  git: "/images/icons/git.svg",
+  github: "/images/icons/github.svg",
+  gulp: "/images/icons/gulp.svg",
+  mysql: "/images/icons/mysql.svg",
+  javascript: "/images/icons/javascript.svg",
+  react: "/images/icons/react.svg",
+  typescript: "/images/icons/typescript.svg",
+  tailwindcss: "/images/icons/tailwind.svg",
+};
